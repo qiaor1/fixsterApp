a22783a898c658a6b972d8ee65c29bd2270e6dd9
Lydia <39285530+qiaor1@users.noreply.github.com>
2021-01-18 13:04:41 -0800

    Fixster part2 : added navigation bar, grid layout, and movie details

---
+++ fixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  fixster
+//
+//  Created by ooo on 1/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
